03ded9849c20d458513e921879bb84ef26e5cd7a
bkucler <benjaminkucler1@gmail.com>
2022-03-22 11:50:37 +0100

    Initial commit

---
+++ src/main/java/com/benjamin/graves/Graves.java
@@ -0,0 +1,17 @@
+package com.benjamin.graves;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Graves extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
